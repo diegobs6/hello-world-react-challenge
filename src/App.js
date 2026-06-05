617499c330fd9198be0839bbc985be5f47c7de7c
deug_ <di.bergeret@duocuc.cl>
2026-06-04 22:26:57 -0400

    Selector de fuente con Google Fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [font, setFont] = useState("Roboto");
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <h1 style={{ fontFamily: font}}>Hello World</h1>
+      <select onChange={(e) => setFont(e.target.value)}>
+        <option value= "Roboto">Roboto</option>
+        <option value= "Playfair Display">Playfair Display</option>
+        <option value= "Pacifico">Pacifico</option>
+        <option value= "Montserrat">Montserrat</option>
+        <option value= "Source Code Pro">Source Code Pro</option>
+        </select>
     </div>
   );
 }
