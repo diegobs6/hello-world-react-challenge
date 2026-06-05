4e1014cba2327e4e5f98f504c3399a4a624dd586
deug_ <di.bergeret@duocuc.cl>
2026-06-05 01:00:57 -0400

    Toggle de negrita

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import { useState } from 'react';
 
 function App() {
   const [font, setFont] = useState("Roboto");
+  const [isBold, setIsBold] = useState(false);
   return (
     <div className="container">
-      <h1 style={{ fontFamily: font}}>Hello World</h1>
+      <h1 style={{ 
+        fontFamily: font,
+        fontWeight: isBold ? "bold" : "normal"
+      }}>Hello World</h1>
       <select onChange={(e) => setFont(e.target.value)}>
         <option value= "Roboto">Roboto</option>
         <option value= "Playfair Display">Playfair Display</option>
         <option value= "Pacifico">Pacifico</option>
         <option value= "Montserrat">Montserrat</option>
         <option value= "Source Code Pro">Source Code Pro</option>
-        </select>
+      </select>
+      <button onClick={() => setIsBold(!isBold)}>
+        {isBold ? "Sacar negrita" : "Negrita"}
+      </button>
     </div>
   );
 }
